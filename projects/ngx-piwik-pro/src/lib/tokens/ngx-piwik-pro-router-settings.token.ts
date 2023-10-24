0fda56828c562de499e742b2b6abc14ecbd975b2
danieltwork <46277472+danieltwork@users.noreply.github.com>
2023-10-25 00:39:35 +0200

    [PPI-494] Audit fix

--- projects/ngx-piwik-pro/src/lib/tokens/ngx-piwik-pro-router-settings.token.ts
+++ projects/ngx-piwik-pro/src/lib/tokens/ngx-piwik-pro-router-settings.token.ts
@@ -1,6 +1,6 @@
 import { InjectionToken } from '@angular/core';
 import { PiwikProRoutingSettings } from '../interfaces/piwik-pro-router-settings.interface';
 
-export const NGX_PIWIK_PRO_ROUTING_SETTINGS_TOKEN = new InjectionToken<PiwikProRoutingSettings>('ngx-google-analytics-routing-settings', {
+export const NGX_PIWIK_PRO_ROUTING_SETTINGS_TOKEN = new InjectionToken<PiwikProRoutingSettings>('ngx-piwik-pro-routing-settings', {
   factory: () => ({})
 });
