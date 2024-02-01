ea08f5b1b4ed01576cf5943c98bea8f5babd27b2
Daniel Twork <46277472+danieltwork@users.noreply.github.com>
2024-02-01 08:17:22 +0100

    fix: add universal support (#25)
    Co-authored-by: nolotz <noah.lotzer@gmail.com>

--- projects/ngx-piwik-pro/src/lib/initializers/piwik-pro.initializer.ts
+++ projects/ngx-piwik-pro/src/lib/initializers/piwik-pro.initializer.ts
@@ -1,5 +1,5 @@
-import { APP_INITIALIZER, FactoryProvider, isDevMode } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { APP_INITIALIZER, FactoryProvider, isDevMode, PLATFORM_ID } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { PiwikProSettings } from '../interfaces/piwik-pro-settings.interface';
 import { NGX_PIWIK_PRO_SETTINGS_TOKEN } from '../tokens/ngx-piwik-pro-settings.token';
 
@@ -10,15 +10,21 @@ export const NGX_PIWIK_PRO_INITIALIZER_PROVIDER: FactoryProvider = {
   deps: [
     NGX_PIWIK_PRO_SETTINGS_TOKEN,
     DOCUMENT,
+    PLATFORM_ID,
   ]
 }
 
 export function PiwikProInitializer(
   settings: PiwikProSettings,
-  document: Document
+  document: Document,
+  platformId: string
 ) {
   window._paq = window._paq || [];
   return async () => {
+    if (!isPlatformBrowser(platformId)) {
+      return;
+    }
+
     if (!settings.containerId) {
       if (!isDevMode()) {
         console.error('Empty tracking code for Piwik Pro. Make sure to provide one when initializing NgxPiwikProModule.');
