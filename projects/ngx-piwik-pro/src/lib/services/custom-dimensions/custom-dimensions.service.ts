9549ccc4345434333c3dc5ba5e287f3a3efdc086
Kamil Grzaba <kamil.grzaba@gmail.com>
2024-07-10 15:27:21 +0200

    custom dimensions with infered parameters

--- projects/ngx-piwik-pro/src/lib/services/custom-dimensions/custom-dimensions.service.ts
+++ projects/ngx-piwik-pro/src/lib/services/custom-dimensions/custom-dimensions.service.ts
@@ -1,19 +1,19 @@
 import { CustomDimensions } from '@piwikpro/tracking-base-library';
 import { Injectable } from '@angular/core';
 
+type ICustomDimensions = typeof CustomDimensions;
+
 @Injectable({
   providedIn: 'root'
 })
 export class CustomDimensionsService {
-  setCustomDimensionValue(customDimensionId: string | number, customDimensionValue: string) {
-    CustomDimensions.setCustomDimensionValue(customDimensionId, customDimensionValue);
-  }
-
   deleteCustomDimension(customDimensionId: string) {
     CustomDimensions.deleteCustomDimension(customDimensionId);
   }
-
   getCustomDimensionValue(customDimensionId: string | number): Promise<string | undefined> {
     return CustomDimensions.getCustomDimensionValue(customDimensionId);
+  }
+  setCustomDimensionValue(...params: Parameters<ICustomDimensions['setCustomDimensionValue']>) {
+    CustomDimensions.setCustomDimensionValue(...params);
   }
 }
