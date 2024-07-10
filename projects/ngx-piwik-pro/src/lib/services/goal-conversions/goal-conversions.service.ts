410bf766a2d38479bb4f0479ad51cb23399e723d
Kamil Grzaba <kamil.grzaba@gmail.com>
2024-07-10 15:59:08 +0200

    goal conversions with infered parameters

--- projects/ngx-piwik-pro/src/lib/services/goal-conversions/goal-conversions.service.ts
+++ projects/ngx-piwik-pro/src/lib/services/goal-conversions/goal-conversions.service.ts
@@ -1,12 +1,13 @@
-import { Dimensions, GoalConversions } from '@piwikpro/tracking-base-library'
-
+import { GoalConversions } from '@piwikpro/tracking-base-library'
 import { Injectable } from '@angular/core';
+
+type IGoalConversions = typeof GoalConversions;
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoalConversionsService {
-  trackGoal(goalId: number | string, conversionValue: number, dimensions?: Dimensions) {
-    GoalConversions.trackGoal(goalId, conversionValue, dimensions)
+  trackGoal( ...params: Parameters<IGoalConversions['trackGoal']>) {
+    GoalConversions.trackGoal(...params)
   }
 }
