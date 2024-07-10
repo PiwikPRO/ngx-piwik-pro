4285016437a54a4f91b9231d8e5d2a2d35819513
Kamil Grzaba <kamil.grzaba@gmail.com>
2024-07-10 15:51:47 +0200

    datalayer with infered parameters

--- projects/ngx-piwik-pro/src/lib/services/data-layer/data-layer.service.ts
+++ projects/ngx-piwik-pro/src/lib/services/data-layer/data-layer.service.ts
@@ -1,11 +1,13 @@
 import { DataLayer } from '@piwikpro/tracking-base-library'
 import { Injectable } from '@angular/core';
 
+type IDataLayer = typeof DataLayer;
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataLayerService {
-  push(data: any) {
-    return DataLayer.push(data);
+  push(...params: Parameters<IDataLayer['push']>) {
+    return DataLayer.push(...params);
   }
 }
