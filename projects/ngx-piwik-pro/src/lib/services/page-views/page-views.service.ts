4db1e7f9d92829868066c0b152358418bf07ce49
Kamil Grzaba <kamil.grzaba@gmail.com>
2024-07-10 16:00:05 +0200

    page views with infered parameters

--- projects/ngx-piwik-pro/src/lib/services/page-views/page-views.service.ts
+++ projects/ngx-piwik-pro/src/lib/services/page-views/page-views.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { PageViews } from '@piwikpro/tracking-base-library'
 
+type IPageViews = typeof PageViews
+
 @Injectable({
   providedIn: 'root'
 })
 export class PageViewsService {
-  trackPageView(customPageTitle?: string) {
-    PageViews.trackPageView(customPageTitle)
+  trackPageView(...params: Parameters<IPageViews['trackPageView']>) {
+    PageViews.trackPageView(...params)
   }
 }
