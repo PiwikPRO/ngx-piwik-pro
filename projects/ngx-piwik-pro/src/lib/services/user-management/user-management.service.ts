f1ef65e99d01cafcaf70220e08f0c36793cdb52a
Szymon Sysło <s.syslo@piwik.pro>
2025-01-13 12:17:08 +0100

    PPI-1357 Update user-management.service

--- projects/ngx-piwik-pro/src/lib/services/user-management/user-management.service.ts
+++ projects/ngx-piwik-pro/src/lib/services/user-management/user-management.service.ts
@@ -1,29 +1,37 @@
 import { Injectable } from '@angular/core';
-import { UserManagement } from '@piwikpro/tracking-base-library'
+import { UserManagement } from '@piwikpro/tracking-base-library';
 
-type IUserManagement = typeof UserManagement
+type IUserManagement = typeof UserManagement;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserManagementService {
   getUserId(...params: Parameters<IUserManagement['getUserId']>) {
-    return UserManagement.getUserId(...params)
+    return UserManagement.getUserId(...params);
   }
 
   setUserId(...params: Parameters<IUserManagement['setUserId']>) {
-    UserManagement.setUserId(...params)
+    UserManagement.setUserId(...params);
   }
 
-  resetUserId(...params: Parameters<IUserManagement['resetUserId']>){
-    UserManagement.resetUserId(...params)
+  resetUserId(...params: Parameters<IUserManagement['resetUserId']>) {
+    UserManagement.resetUserId(...params);
   }
 
   getVisitorId(...params: Parameters<IUserManagement['getVisitorId']>) {
-    return UserManagement.getVisitorId(...params)
+    return UserManagement.getVisitorId(...params);
   }
 
   getVisitorInfo(...params: Parameters<IUserManagement['getVisitorInfo']>) {
-    return UserManagement.getVisitorInfo(...params)
+    return UserManagement.getVisitorInfo(...params);
+  }
+  setUserIsAnonymous(
+    ...params: Parameters<IUserManagement['setUserIsAnonymous']>
+  ) {
+    UserManagement.setUserIsAnonymous(...params);
+  }
+  deanonymizeUser() {
+    UserManagement.deanonymizeUser();
   }
 }
