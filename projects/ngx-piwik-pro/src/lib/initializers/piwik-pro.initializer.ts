312b8609d670c7155230ec75f0f41c77ee74c9e9
auto200 <49625375+auto200@users.noreply.github.com>
2025-02-25 13:23:58 +0100

    revert format

--- projects/ngx-piwik-pro/src/lib/initializers/piwik-pro.initializer.ts
+++ projects/ngx-piwik-pro/src/lib/initializers/piwik-pro.initializer.ts
@@ -1,25 +1,21 @@
-import {
-  APP_INITIALIZER,
-  FactoryProvider,
-  PLATFORM_ID,
-  isDevMode,
-} from '@angular/core';
+import { APP_INITIALIZER, FactoryProvider, PLATFORM_ID, isDevMode } from '@angular/core';
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
-import PiwikPro, {
-  DataLayer,
-  Miscellaneous,
-} from '@piwikpro/tracking-base-library';
+import PiwikPro, { DataLayer, Miscellaneous } from '@piwikpro/tracking-base-library';
 
 import { NGX_PIWIK_PRO_SETTINGS_TOKEN } from '../tokens/ngx-piwik-pro-settings.token';
 import { PiwikProSettings } from '../interfaces/piwik-pro-settings.interface';
-import pkg from '../../../package.json';
+import pkg from "../../../package.json";
 
 export const NGX_PIWIK_PRO_INITIALIZER_PROVIDER: FactoryProvider = {
   provide: APP_INITIALIZER,
   multi: true,
   useFactory: PiwikProInitializer,
-  deps: [NGX_PIWIK_PRO_SETTINGS_TOKEN, DOCUMENT, PLATFORM_ID],
-};
+  deps: [
+    NGX_PIWIK_PRO_SETTINGS_TOKEN,
+    DOCUMENT,
+    PLATFORM_ID,
+  ]
+}
 
 export function PiwikProInitializer(
   settings: PiwikProSettings,
@@ -28,7 +24,7 @@ export function PiwikProInitializer(
 ) {
   if (window) {
     window._paq = window._paq || [];
-    Miscellaneous.setTrackingSourceProvider('angular', pkg.version);
+    Miscellaneous.setTrackingSourceProvider("angular", pkg.version)
   }
   return async () => {
     if (!isPlatformBrowser(platformId)) {
@@ -37,9 +33,7 @@ export function PiwikProInitializer(
 
     if (!settings.containerId) {
       if (!isDevMode()) {
-        console.error(
-          'Empty tracking code for Piwik Pro. Make sure to provide one when initializing NgxPiwikProModule.'
-        );
+        console.error('Empty tracking code for Piwik Pro. Make sure to provide one when initializing NgxPiwikProModule.');
       }
 
       return;
@@ -47,9 +41,7 @@ export function PiwikProInitializer(
 
     if (!settings.containerURL) {
       if (!isDevMode()) {
-        console.error(
-          'Empty tracking URL for Piwik Pro. Make sure to provide one when initializing NgxPiwikProModule.'
-        );
+        console.error('Empty tracking URL for Piwik Pro. Make sure to provide one when initializing NgxPiwikProModule.');
       }
 
       return;
@@ -57,29 +49,27 @@ export function PiwikProInitializer(
 
     if (!document) {
       if (!isDevMode()) {
-        console.error(
-          'Was not possible to access Document interface. Make sure this module is running on a Browser w/ access do Document interface.'
-        );
+        console.error('Was not possible to access Document interface. Make sure this module is running on a Browser w/ access do Document interface.');
       }
     }
 
-    if (settings.dataLayerName) {
-      DataLayer.setDataLayerName(settings.dataLayerName);
+    if(settings.dataLayerName){
+        DataLayer.setDataLayerName(settings.dataLayerName)
     }
 
     const s: HTMLScriptElement = document.createElement('script');
     s.async = true;
     if (settings.nonce) {
-      s.setAttribute('nonce', settings.nonce);
+      s.setAttribute("nonce", settings.nonce);
     }
     s.text = PiwikPro.getInitScript({
       containerId: settings.containerId,
       containerUrl: settings.containerURL,
       nonceValue: settings.nonce,
-      dataLayerName: settings.dataLayerName,
-    });
+      dataLayerName: settings.dataLayerName
+    })
 
     const head: HTMLHeadElement = document.getElementsByTagName('head')[0];
     head.appendChild(s);
-  };
+  }
 }
